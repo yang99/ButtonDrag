55d03a62d6f409b919c45c31f3c43c642718de59
yang99 <996212017@qq.com>
2015-10-26 11:45:30 +0800

    Initial Commit

---
+++ ButtonDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonDrag
+//
+//  Created by yangyao on 15/10/26.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonDrag
+//
+//  Created by yangyao on 15/10/26.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
